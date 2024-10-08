9d823187ae6e016fa1cbc8e40c2aa7e62113eafa
Acer <gugunmfauzi16@gmail.com>
2024-10-08 20:48:48 +0700

    Melakukan Scraping dari BMKG

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ Modularisasi dengan Package
 import deteksigempa
 
 if __name__ == '__main__':
-    gempa_di_indonesia = deteksigempa('https://bmkg.go.id')
-    print(f'Aplikasi utama menggunakan package yg memiliki deskripsi {deteksigempa.Desciption}')
+    gempa_di_indonesia = deteksigempa.deteksigempa('https://bmkg.go.id')
+    print(f'Aplikasi utama menggunakan package yg memiliki deskripsi {gempa_di_indonesia.Description}')
     gempa_di_indonesia.tampilkan_keterangan()
     gempa_di_indonesia.run()
